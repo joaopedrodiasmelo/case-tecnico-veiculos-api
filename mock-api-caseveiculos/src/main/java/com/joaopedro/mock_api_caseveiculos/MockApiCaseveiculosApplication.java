44c1963a45bca00a42971eb19ce90fb84b44555b
joao pedro <dias.jpmelo@usp.br>
2025-03-03 18:59:19 -0300

    feat: inicializa o projeto

---
+++ mock-api-caseveiculos/src/main/java/com/joaopedro/mock_api_caseveiculos/MockApiCaseveiculosApplication.java
@@ -0,0 +1,14 @@
+package com.joaopedro.mock_api_caseveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockApiCaseveiculosApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MockApiCaseveiculosApplication.class, args);
+	}
+
+}
---
+++ mock-api-caseveiculos/src/test/java/com/joaopedro/mock_api_caseveiculos/MockApiCaseveiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaopedro.mock_api_caseveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockApiCaseveiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
